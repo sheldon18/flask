a47eab74549523ee1e742c88e6f84dd2ed6cd1e1
Ubuntu <ubuntu@ip-172-31-87-185.ec2.internal>
2019-10-13 08:46:13 +0000

    added home page and about page

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask     # captial F indicates that its a class name
+from flask import Flask, render_template     # captial F indicates that its a class name
 
 app = Flask(__name__)       #in Flask, convention is that our variable is called app
 
 @app.route("/")             #decorator, alwasy starts with an @ sign and aka pie notation, it is a way of wrapping functions
 def index():
-    return "Hello, World"
+    return render_template("index.html")
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
